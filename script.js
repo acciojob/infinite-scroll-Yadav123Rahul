70cc2c97542a244094ab141c082a1acd17479567
Rahul Yadav <119389899+Yadav123Rahul@users.noreply.github.com>
2023-09-29 13:50:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const list = document.getElementById("infi-list");
+  const totalItems = 10; // Total items initially
+
+  // Add initial list items
+  for (let i = 1; i <= totalItems; i++) {
+    const listItem = document.createElement("li");
+    listItem.textContent = `Item ${i}`;
+    list.appendChild(listItem);
+  }
+
+  // Function to check if the user has reached the end of the list
+  function isEndOfList() {
+    const lastItem = list.lastElementChild;
+    const lastItemRect = lastItem.getBoundingClientRect();
+    return lastItemRect.bottom <= window.innerHeight;
+  }
+
+  // Function to add more list items
+  function addMoreItems() {
+    const itemsToAdd = 2; // Number of items to add
+    for (let i = 1; i <= itemsToAdd; i++) {
+      const listItem = document.createElement("li");
+      listItem.textContent = `Item ${totalItems + i}`;
+      list.appendChild(listItem);
+    }
+    totalItems += itemsToAdd;
+  }
+
+  // Event listener for scrolling
+  window.addEventListener("scroll", function () {
+    if (isEndOfList()) {
+      addMoreItems();
+    }
+  });
+});
 
